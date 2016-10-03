3faa95ed0f3025176ab3f87364aedb5df74ba935
ankitkr3 <ankitkr035@gmail.com>
2016-10-03 17:16:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,12 +6,12 @@ var element = document.getElementById('main-text');
 element.innerHTML = 'new value';
 
 // move the image
-var image = document.getElementById('madi');
+var img = document.getElementById('madi');
 var marginLeft = 0;
 function moveRight() {
     marginLeft = marginLeft + 1;
     img.style.marginLeft = marginLeft + 'px';
 }
 img.onclick = function () {
-    var interval = setinterval(moveRight,100);
+    var interval = setInterval(moveRight,50);
 };
